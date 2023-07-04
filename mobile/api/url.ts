4e4f212098224cb04857df8089422a3bccee95a2
Mario Merendino <mariomerendino@gmail.com>
2023-07-03 22:30:43 -0400

    add prod url

--- mobile/api/url.ts
+++ mobile/api/url.ts
@@ -1,6 +1,10 @@
 import {Platform} from 'react-native'
 
+const isProd = false;
 export const BaseURL = () => {
+  if(isProd) {
+    return "https://liftingbuddy-91d262180055.herokuapp.com"
+  }
   if(Platform.OS === 'ios') {
     return "http://localhost:3000"
   } else {
