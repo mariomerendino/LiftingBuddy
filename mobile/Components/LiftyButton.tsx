258a093aa80f06b4533cfa35ed5bb5906f3a121a
Mario Merendino <mariomerendino@gmail.com>
2023-07-21 15:23:23 -0400

    create lifty secondary button

--- mobile/Components/LiftyButton.tsx
+++ mobile/Components/LiftyButton.tsx
@@ -5,15 +5,22 @@ interface Props {
   onPress: () => void;
   text: string;
   disabled?: boolean;
+  type?: "primary" | "secondary";
 }
 
-const LiftyButton = ({ onPress, text, disabled = false }: Props) => {
+const LiftyButton = ({
+  onPress,
+  text,
+  disabled = false,
+  type = "primary",
+}: Props) => {
   return (
     <Pressable
       disabled={disabled}
       onPress={onPress}
       style={({ pressed }) => [
         styles.base,
+        styles[type],
         pressed && styles.pressed,
         disabled && styles.disabled,
       ]}
@@ -28,11 +35,16 @@ const styles = StyleSheet.create({
     alignContent: "center",
     alignItems: "center",
     justifyContent: "center",
-    backgroundColor: "#ADD8E6",
     paddingLeft: 20,
     paddingRight: 20,
     borderRadius: 20,
     height: 40,
+  },
+  primary: {
+    backgroundColor: "#ADD8E6",
+  },
+  secondary: {
+    backgroundColor: "#D3D3D3",
   },
   pressed: {
     opacity: 0.8,
